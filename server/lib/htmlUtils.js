a309c34427c1a5644ee6f9af15df0f306badb83f
Michal Bláha <misablaha@gmail.com>
2018-11-11 17:26:52 +0100

    Save searched.html based on environment

--- server/lib/htmlUtils.js
+++ server/lib/htmlUtils.js
@@ -5,7 +5,14 @@ const { serializeToString } = require('xmlserializer');
 const { DOMParser } = require('xmldom');
 
 exports.parseDom = html => {
-  fs.writeFileSync(path.resolve(__dirname, '../../public/searched.html'), html);
+  const filename = process.env.NODE_ENV !== 'development'
+    // It must be out of watched folders because of babel hot reload
+    ? path.resolve(__dirname, '../../searched.html')
+    // Otherwise, save to the 'public' folder due to availability through the http
+    : path.resolve(__dirname, '../../public/searched.html');
+
+  fs.writeFileSync(filename, html);
+
   const document = parse5.parse(html);
   const xhtml = serializeToString(document);
   return new DOMParser().parseFromString(xhtml);
